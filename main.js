aee47627c010c0990ded3dfd52e26bd4744ec8e9
Masha <masha@MacBook-Air.local>
2024-04-28 19:10:27 +0200

    Array Shuffling

--- main.js
+++ main.js
@@ -36,3 +36,15 @@ function chunkArray (array, chunk) {
 
   return res;
 }
+
+//Task 3: Array Shuffling
+function customShuffle(array) {
+  const result = array.slice();
+
+  for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1)); 
+      [result[i], result[j]] = [result[j], result[i]];
+  }
+
+  return result;
+}
