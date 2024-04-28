f629f963b6ca8c6297435d21a15488a6587e2f17
Masha <masha@MacBook-Air.local>
2024-04-28 18:33:19 +0200

    customFilterUnique fixed

--- main.js
+++ main.js
@@ -7,10 +7,20 @@ function customFilterUnique (array, callback) {
     const current = callback(array[i]);
 
     if (!filteredSet.has(current)){
-      filteredArray.push(current);
+      filteredArray.push(array[i]);
       filteredSet.add(current);
     }
   }
   
   return filteredArray;
 }
+
+const arrayOfObjects = [
+  { name: "a", cost: 1 },
+  { name: "a", cost: 2  },
+  { name: "b", cost: 3  },
+  { name: "b", cost: 4  },
+  { name: "d", cost: 5  }
+];
+
+console.log(customFilterUnique(arrayOfObjects, obj => obj.name));
